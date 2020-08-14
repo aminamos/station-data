9a2d441292051e5a0cfa97fc781f766579b201b0
aminamos <26092352+aminamos@users.noreply.github.com>
2020-08-14 15:01:28 -0700

    comment code

--- scraper-draft.py
+++ scraper-draft.py
@@ -5,33 +5,55 @@ from datetime import datetime, time, date
 import time
 import requests
 
+# date to begin looking for data
 start_date = "2017-03-01"
+
+# date to stop looking for data
 end_date = "2017-03-31"
+
+# create a range of dates from
+# start to end date
 start = parser.parse(start_date)
 end = parser.parse(end_date)
 dates = list(rrule.rrule(rrule.DAILY, dtstart=start, until=end))
 date_list = []
 
+# store a list of properly formatted dates
 for date in dates:
     date_list.append(date.strftime("%Y-%m-%d"))
 
+# Enters the context of the site list CSV
 with open('station-list-draft.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
+
+    # For each site...
     for row in csv_reader:
+
+        # Store details for the site
         site_name = row["\ufeffSite Name"]
         site_code = row["Code"]
         loc_lat = row["Lat"]
         loc_long = row["Long"]
 
+        # For each date in the date list...
         for date in date_list:
             url = f"https://www.wunderground.com/dashboard/pws/{site_code}/table/{date}/{date}/daily"
+
+            # "Go to" the web page
             response = requests.get(url)
             html = response.text
             soup = BeautifulSoup(html, "html.parser")
+
+            # Find the tables on the page
             table = soup.find_all("table")
+
+            # Target data is in the 4th table.
+            # If they ever add or delete tables,
+            # this script needs to be udpated.
             table = table[3]
 
             output_rows = []
+
             header_row = table.findAll('tr')[0].findAll('th')
             header_output_row = []
 
