7f7a5fc77aef084263873945b2143689191aac21
aminamos <26092352+aminamos@users.noreply.github.com>
2020-08-13 17:08:50 -0700

    formatting

--- scraper.py
+++ scraper.py
@@ -13,24 +13,24 @@ dates = list(rrule.rrule(rrule.DAILY, dtstart=start, until=end))
 date_list = []
 
 for date in dates:
-  date_list.append(date.strftime("%Y-%m-%d"))
+    date_list.append(date.strftime("%Y-%m-%d"))
 
 for date in date_list:
-  url = f"https://www.wunderground.com/dashboard/pws/KWASEATT611/table/{date}/{date}/daily"
-  response = requests.get(url)
-  html = response.text
-  soup = BeautifulSoup(html, "html.parser")
-  table = soup.find_all("table")
-  table = table[3]
+    url = f"https://www.wunderground.com/dashboard/pws/KWASEATT611/table/{date}/{date}/daily"
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find_all("table")
+    table = table[3]
 
-  output_rows = []
-  for table_row in table.findAll('tr'):
-    columns = table_row.findAll('td')
-    output_row = []
-    for column in columns:
-        output_row.append(column.text)
-    output_rows.append(output_row)
-      
-  with open(f'{date}.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(output_rows)
+    output_rows = []
+    for table_row in table.findAll('tr'):
+        columns = table_row.findAll('td')
+        output_row = []
+        for column in columns:
+            output_row.append(column.text)
+        output_rows.append(output_row)
+
+    with open(f'{date}.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(output_rows)
